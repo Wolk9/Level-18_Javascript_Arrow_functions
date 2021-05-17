d2f1fdf133f177a52bab99e4500763723a6b1b7a
Wolk9 <66083478+Wolk9@users.noreply.github.com>
2021-05-17 17:23:13 +0200

    Learing file

---
+++ script.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function isPositive(number) {
+  return number >= 0;
+}
+
+function randomNumber() {
+  return Math.random;
+}
+
+document.addEventListener("click", function () {
+  console.log("click");
+});
